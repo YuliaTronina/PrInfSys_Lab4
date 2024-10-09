9042da4b61ad7ec75ea79479009b56dbfb42eb14
Ars <122453713+ArseniyCherpak@users.noreply.github.com>
2024-10-09 05:23:55 +0500

    Задание 1 Программа 1

---
+++ IandD/src/main/java/org/example/Main.java
@@ -0,0 +1,21 @@
+// This is a personal academic project. Dear PVS-Studio, please check it.
+
+// PVS-Studio Static Code Analyzer for C, C++, C#, and Java: https://pvs-studio.com
+
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        Numbers.sample1();
+        System.out.println();
+
+        Numbers.sample2();
+        System.out.println();
+
+        Numbers.getVal(-1);
+        System.out.println();
+
+    }
+}
