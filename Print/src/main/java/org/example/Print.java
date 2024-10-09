2045c91e090832ae6538bf1c256eba41aae02d13
Ars <122453713+ArseniyCherpak@users.noreply.github.com>
2024-10-09 05:40:33 +0500

    Задание 1 Программа 4

---
+++ Print/src/main/java/org/example/Print.java
@@ -0,0 +1,12 @@
+// This is a personal academic project. Dear PVS-Studio, please check it.
+
+// PVS-Studio Static Code Analyzer for C, C++, C#, and Java: https://pvs-studio.com
+
+package org.example;
+
+public class Print {
+    public static void sample1(){
+        System.out.printf("%s\n", "str#1");
+        System.out.println("str#2");
+    }
+}
